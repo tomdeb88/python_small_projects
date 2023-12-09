a639a1078db9eff9fc03d59228eec5b938d8e380
Tomasz Debowski <tomaszdebowski88@gmail.com>
2023-12-10 00:20:25 +0100

    final changes

--- hangman/app.py
+++ hangman/app.py
@@ -1,28 +1,36 @@
 import random
 import hangman_steps
 import word_list
+import os
+
+
 
 print(hangman_steps.logo,'\n\n')
+
 #choosing a word
 word_list=word_list.word_list
 chosen_word=word_list[random.randint(0,len(word_list)-1)]
+print(chosen_word)
 
 display=[]
 chances=6
 game_run=True
+clear=lambda: os.system('clear')
 
-#display cover word
+#display covered word
 for _ in range(len(chosen_word)):
     display.append(' _ ')
 
-print(f"\n\n{''.join(display)}\n\n")
+print(f"\n\n{' '.join(display)}\n\n")
 
 
 while game_run:
     #get user's guess
     guessed_letter=input('Guess a letter: ').lower()
-    
     #check is it already in display list
+   
+    clear()
+    
     if guessed_letter  in display:
         print("!!! You have already given this letter !!!")
 
@@ -32,10 +40,14 @@ while game_run:
             for index,letter in enumerate(chosen_word):
                 if guessed_letter==letter:
                     display[index]=letter
-    print(f"{''.join(display)}")
+    
+    
+    print(f"\n\n{''.join(display)}\n\n")
+   
 
     if guessed_letter not in chosen_word:
         chances-=1
+    if chances<6:
         print(hangman_steps.stages[chances])
 
     if chances==0:
