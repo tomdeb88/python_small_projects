975cd198ddf1e54aaa6f416719ca931d6d0af2d1
Tomasz Debowski <tomaszdebowski88@gmail.com>
2023-12-10 20:10:08 +0100

    last changes

--- hangman/app.py
+++ hangman/app.py
@@ -24,14 +24,18 @@ print(f"\n\n{' '.join(display)}\n\n")
 
 
 while game_run:
+    
+    
     #get user's guess
     guessed_letter=input('Guess a letter: ').lower()
-    
-   
     clear()
+    
+    
     #check is it already in display list
     if guessed_letter  in display:
         print("!!! You have already given this letter !!!")
+        continue
+    
 
     #check if the letter is in the word and replace underscores with proper letters
     for l in chosen_word:
@@ -40,21 +44,25 @@ while game_run:
                 if guessed_letter==letter:
                     display[index]=letter
     
-    
-    print(f"\n\n{''.join(display)}\n\n")
    
+    # letter is not int the word
 
     if guessed_letter not in chosen_word:
         chances-=1
     if chances<6:
         print(hangman_steps.stages[chances])
 
+    print(f"\n\n{''.join(display)}\n\n")
+        
+   
+    # ending game, final statement
     if chances==0:
-        print('-------> You loose <-------')
+        print(f'-------> You loose <-------\n\n{chosen_word}')
         game_run=False
     elif " _ " not in display:
         print('-------> You won <-------')
         game_run=False
+    
 
 
 
