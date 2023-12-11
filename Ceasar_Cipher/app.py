ebb41f8873b04d54184c54aec3138bcd73ea8853
Tomasz Debowski <tomaszdebowski88@gmail.com>
2023-12-11 23:49:23 +0100

    adding encrypt function

--- Ceasar_Cipher/app.py
+++ Ceasar_Cipher/app.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction=input("Type 'encode' to encrypt or typr 'decode' to decrypt:\n ")
 text=input('Type your message here:\n').lower()
@@ -8,11 +8,11 @@ shift=int(input('Type the shift number:\n'))
 
 def encrypt(message,shift):
     encrypted=''
-    for l in text:
+    for l in message:
         index=alphabet.index(l)
         letter=alphabet[index+shift]
         encrypted+=letter
-    print(encrypted)
+    return encrypted
 
-encrypt(text,shift)
-    
+word=encrypt(text,shift)
+print(word)
