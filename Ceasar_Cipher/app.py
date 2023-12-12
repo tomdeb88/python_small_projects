18ba82cbd4508975b268aefe6fc933f81a10dc4e
Tomasz Debowski <tomaszdebowski88@gmail.com>
2023-12-12 21:23:14 +0100

    adding decrypt function

--- Ceasar_Cipher/app.py
+++ Ceasar_Cipher/app.py
@@ -1,18 +1,38 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction=input("Type 'encode' to encrypt or typr 'decode' to decrypt:\n ")
+direction=input("Type 'encode' to encrypt or typr 'decode' to decrypt:\n ").lower()
 text=input('Type your message here:\n').lower()
 shift=int(input('Type the shift number:\n'))
 
+word=None 
+
+if direction=='encode':
+
+    def encrypt(message,shift):
+        encrypted=''
+        for l in message:
+            index=alphabet.index(l)
+            letter=alphabet[index+shift]
+            encrypted+=letter
+        return encrypted
+    word=encrypt(text,shift)
+    print(word)
 
 
-def encrypt(message,shift):
-    encrypted=''
-    for l in message:
-        index=alphabet.index(l)
-        letter=alphabet[index+shift]
-        encrypted+=letter
-    return encrypted
 
-word=encrypt(text,shift)
-print(word)
+elif direction=='decode':
+    def decrypt(text_decrypted,shift):
+
+        for l in text_decrypted:
+            decrypted=''
+            index=alphabet.index(l)
+            letter=alphabet[index-shift]
+            decrypted+=letter
+            return decrypted
+
+    decrypted_word=decrypt(word,shift)
+    print(decrypted_word)
+
+
+
+
