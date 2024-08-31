7ca2353b00c0ff32873c4d51dee599abb4026836
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-08-31 23:30:33 +0200

    add calculate funtcion

--- blackjack/app.py
+++ blackjack/app.py
@@ -2,20 +2,50 @@ from blackjack_logo import logo
 import random
 
 cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+player_cards=[]
+comp_cards=[]
+
+
 
 def calculating_score():
-    player_cards=[]
-    for card in range(2):
+    if sum(player_cards)==0 and sum(comp_cards)==0:
+        for _ in range(2):
+            player_cards.append(random.choice(cards))
+            comp_cards.append(random.choice(cards))
+    else:
         player_cards.append(random.choice(cards))
-    print(player_cards,sum(player_cards))
+    if sum(comp_cards)<=16:
+        comp_cards.append(random.choice(cards))
 
 
-will_play=input("Do you want to play a game of Blackjack? type 'y' or 'no': ")
+print(logo,"\n\n")
 
-if will_play=='y':
-    print(logo,"\n\n")
-    calculating_score()
+calculating_score()
+print(f"Your cards {player_cards},your current score: {sum(player_cards)}")
+print(f"Computer's first card:{comp_cards[0],comp_cards}")
+
+if sum(player_cards)==21:
+    print("you win")
+    print(f"Your cards {player_cards},your current score: {sum(player_cards)}")
+    print(f"Computer's first card:{comp_cards}")
+elif sum(player_cards)==21 and sum(comp_cards)==21:
+    print("It's a draw")
+    print(f"Your cards {player_cards},your current score: {sum(player_cards)}")
+    print(f"Computer's first card:{comp_cards}")
+elif sum(comp_cards)==21:
+    print('You lose!')
+    print(f"Your cards {player_cards},your current score: {sum(player_cards)}")
+    print(f"Computer's first card:{comp_cards}")
+
+
+another_card=input("Type 'y' for another card or 'n' to pass: ")
+
+
+
+
 
     
-else:
-    exit()
+
+
+
+    
