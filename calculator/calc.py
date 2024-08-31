287c1d3f2e6af4558974b20169085b4892f5e188
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-08-31 20:03:00 +0200

    add ascii art

---
+++ calculator/calc.py
@@ -0,0 +1,65 @@
+import os
+
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(n1,n2):
+    return n1 / n2
+
+clear=lambda:os.system('clear')
+
+dict={"+":add,
+      "-":subtract,
+      "*":multiply,
+      "/":divide,}
+
+
+def calculating(number1,number2,operation):
+    return dict[operation](number1,number2)
+
+
+
+
+
+while True:
+    first_number=float(input("What's  the first number?: "))
+    operator=input("+\n-\n*\n/\nPick an operator: ")
+    second_number=float(input("What's the next number?: "))
+
+    result=calculating(first_number,second_number,operator)
+
+    print(f'{first_number} {operator} {second_number} = {result}')
+
+    keep_going=input(f"Type 'y' to  continue with {result}, or typr 'n' to start a new calculation:").lower()
+
+
+
+    if keep_going=='y':
+        while True:
+            first_number=result
+            operator=input("+\n-\n*\n/\nPick an operator: ")
+            second_number=float(input("What's the next number?: "))
+            result=calculating(first_number,second_number,operator)
+            print(f'{first_number} {operator} {second_number} = {result}')
+            keep_going=input(f"Type 'y' to  continue with {result}, or typr 'n' to start a new calculation:").lower()
+            if keep_going=='n':
+                clear()
+                break
+    else:
+        clear()
+        continue           
+    
+        
+
+
+
+
+    
+
+
