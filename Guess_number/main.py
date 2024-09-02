0381075316fc315774ff3150d4014d9df790c3b0
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-09-03 00:05:40 +0200

    ending

--- Guess_number/main.py
+++ Guess_number/main.py
@@ -1,6 +1,7 @@
 import random
 
 random_num=random.randint(0,101)
+end=False
 
 print("I'm thinking of a number between 1 and 100")
 difficulty=input("Choose a difficulty.Type 'easy' or 'hard':").lower()
@@ -8,6 +9,32 @@ difficulty=input("Choose a difficulty.Type 'easy' or 'hard':").lower()
 if difficulty=='easy':
     attempts=10
 elif difficulty=='hard':
-    attempts=10
+    attempts=5
+
+def compare(num):
+    global attempts,end
+    if num<random_num:
+        print("too low")
+        attempts-=1
+
+    elif num >random_num:
+        print("too high")
+        attempts-=1
+
+    elif num==random_num:
+        print(f"You git it! The number is {random_num}")
+        end=True
+        exit()
+   
+
+
+while not end:
+    print(f"You have {attempts} attempts remaining to guess the number.")
+    guess=int(input("Make a guess: "))
+    compare(guess)
+    if attempts==0:
+        print(f'Game over.Number is {random_num}')
+        end=True
+
 
 
