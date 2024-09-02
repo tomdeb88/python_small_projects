093d763d38be6256ffed095efa1412a1daa74d83
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-09-02 22:17:20 +0200

    finishing project

--- blackjack/app.py
+++ blackjack/app.py
@@ -1,51 +1,87 @@
-from blackjack_logo import logo
 import random
+from blackjack_logo import logo
+import os
 
-cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
-player_cards=[]
-comp_cards=[]
+clear=lambda:os.system('clear')
 
 
+def deal_card():
+    """picking up a random card"""
+    cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card=random.choice(cards)
+    return card
 
-def calculating_score():
-    if sum(player_cards)==0 and sum(comp_cards)==0:
-        for _ in range(2):
-            player_cards.append(random.choice(cards))
-            comp_cards.append(random.choice(cards))
+def calculating_score(cards):
+    if sum(cards)==21 and len(cards)==2:
+        return 0
+    if 11 in cards and sum(cards)>21:
+        cards.remove(11)
+        cards.append(1)
+    
+    return sum(cards)
+               
+def compare(user_score,comp_score):
+    if user_score==comp_score:
+        return 'Draw'
+    elif comp_score==0:
+        return 'You lose'
+    elif user_score==0:
+        return "You win"
+    elif user_score>21:
+        return "You went over. You lose"
+    elif comp_score>21:
+        return "You win,dealer went over"
+    elif user_score>comp_score:
+        return "You win"
     else:
-        player_cards.append(random.choice(cards))
-    if sum(comp_cards)<=16:
-        comp_cards.append(random.choice(cards))
-
-
-print(logo,"\n\n")
-
-calculating_score()
-print(f"Your cards {player_cards},your current score: {sum(player_cards)}")
-print(f"Computer's first card:{comp_cards[0],comp_cards}")
-
-if sum(player_cards)==21:
-    print("you win")
-    print(f"Your cards {player_cards},your current score: {sum(player_cards)}")
-    print(f"Computer's first card:{comp_cards}")
-elif sum(player_cards)==21 and sum(comp_cards)==21:
-    print("It's a draw")
-    print(f"Your cards {player_cards},your current score: {sum(player_cards)}")
-    print(f"Computer's first card:{comp_cards}")
-elif sum(comp_cards)==21:
-    print('You lose!')
-    print(f"Your cards {player_cards},your current score: {sum(player_cards)}")
-    print(f"Computer's first card:{comp_cards}")
-
-
-
-
-
-
-
-
+        return "You lose"
     
+def play_game():
+    print(logo)
+    user_cards=[]
+    computer_cards=[]
+    user_score=-1
+    computer_score=-1
+    is_game_over=False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not is_game_over:
+        user_score=calculating_score(user_cards)
+        computer_score=calculating_score(computer_cards)
+        print(f"Your cards: {user_cards}, your score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+        if user_score==0 or computer_score==0 or user_score>21:
+            is_game_over=True
+        else:
+            another_card=input("Type 'y' to get another card, type 'n' to pass: ")
+            if another_card=='y':
+                user_cards.append(deal_card())
+            else:
+                is_game_over=True
+
+    while computer_score!=0 and computer_score<17:
+        computer_cards.append(deal_card())
+        computer_score=calculating_score(computer_cards)
+
+    print(f"Your final cards: {user_cards}, your score: {user_score}")
+    print(f"Computer's final cards: {computer_cards}, computer's score {computer_score}")
+    print(compare(user_score,computer_score))
+
+
+while input("Do you want to play a game of blackjack? Type 'y' or 'n': ").lower()=='y':
+    clear()
+    play_game()
 
 
 
-    
+
+
+
+
+
+
+
