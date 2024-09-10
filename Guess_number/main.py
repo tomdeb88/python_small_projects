7d1e97393030e7fa607762289b3633fafc911caf
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-09-10 17:53:52 +0200

    full project

--- Guess_number/main.py
+++ Guess_number/main.py
@@ -1,11 +1,11 @@
 import random
-from art import logo
+import art
 
 random_num=random.randint(1,100)
 attempts=0
 end=False
 
-print(logo)
+print(art.logo)
 print("I'm thinking of a number between 1 and 100")
 difficulty=input("Choose a difficulty.Type 'easy' or 'hard':").lower()
 
