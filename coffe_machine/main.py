e423400e929c1b5d0342a3a52564d5862a2e4747
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-09-13 21:42:59 +0200

    add payment functionality

--- coffe_machine/main.py
+++ coffe_machine/main.py
@@ -11,6 +11,20 @@ resources={
 def show_resources():
     return f"Water: {resources['water']} ml\nMilk: {resources['milk']} ml\nCoffee: {resources['coffee']} g\nBudget: ${budget}"
 
+def processing_coins(drink,money,MENU):
+    print("Please insert coins")
+    quartes=int(input("How many quarters: "))
+    dimes=int(input("How many dimes?: "))
+    nickles=int(input("How many nickles?: "))
+    pennies=int(input("How many pennies?: "))
+    total=(quartes*0.25+dimes*0.10+nickles*0.05+pennies*0.01)+money
+    if MENU[drink]['cost']>total:
+        print('Not enough money',total,budget)
+    else:
+        print(total,budget)
+        return round(total - MENU[drink]['cost'],2)
+        
+
 
 
 while not off:
@@ -24,6 +38,7 @@ while not off:
         if resources['water']>=50 and resources['coffee']>=18:
             resources['water']-=50
             resources['coffee']-=18
+            budget=processing_coins(response,budget,MENU)
             print("there is your drink")
         else:
             print("Not enough ingredients")
