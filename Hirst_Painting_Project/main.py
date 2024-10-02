1466ee77694aefe2620bacbedb40f468bd895139
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-10-02 19:34:56 +0200

    first commit

---
+++ Hirst_Painting_Project/main.py
@@ -0,0 +1,32 @@
+# import colorgram
+from turtle import Turtle,Screen
+
+# colors=colorgram.extract('image.jpg',15)
+#
+# list_of_colors=[]
+#
+#
+#
+#
+# for color in colors:
+#     r=color.rgb.r
+#     g=color.rgb.g
+#     b=color.rgb.b
+#     color_tuple=(r,g,b)
+#     list_of_colors.append(color_tuple)
+
+color_list=[(199, 175, 117), (125, 36, 24), (187, 158, 51), (170, 106, 56), (6, 57, 83), (222, 223, 225),
+            (199, 215, 204),(108, 67, 85), (88, 142, 56), (20, 122, 175), (110, 160, 175), (76, 39, 48)]
+
+tim=Turtle()
+screen=Screen()
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
