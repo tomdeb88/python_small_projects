a4621a8f4ae7ca1016b318b20118b5acc94ba00f
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-10-14 15:59:17 +0200

    first commit

---
+++ Pong/main.py
@@ -0,0 +1,23 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=800 , height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
